7c3a143a44f35339cd336c844ad607b701b3d2ef
Adel Mahmoud <33881557+adelmaek@users.noreply.github.com>
2018-04-23 21:13:52 +0200

    Update SocialNetwork.java

--- Adel Mahmoud/SocialNetwork.java
+++ Adel Mahmoud/SocialNetwork.java
@@ -1,5 +1,4 @@
 
-
 package socialnetwork;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -23,7 +22,7 @@ import socialnetwork.Post;
 
 public class SocialNetwork extends Application {
 
-     
+     Stage window;
      Label userNameLabel;
      Button homeButton;
      Button profileButton;
@@ -39,108 +38,18 @@ public class SocialNetwork extends Application {
      ScrollPane CenterPostsPane;
      Group postsGroup;
      Button searchButton;
+      Scene homePageScene;
+      Scene profileScene;
      
     
     public static void main(String[] args) {
          launch(args);
          
     }
-    void setCenterPane()
+   
+   //***********HOME Page Functions****************************************************
+    public  Scene homePage (user currentUser)
     {
-        centerPane = new Pane();
-        centerPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
-        + "-fx-border-width:0;" + "-fx-border-insets: 0;"
-       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #ECF0F1;"+"flex-direction: column;");
-        borderPane.setCenter(centerPane);
-        
-        statusBar = new TextField();
-        statusBar.setPrefSize(1000, 60); 
-        statusBar.setLayoutX(10);
-        statusBar.setLayoutY(10); 
-        statusBar.setPromptText("What's in your mind?...");
-        centerPane.getChildren().add(statusBar);
-        
-        postStatusFromStatusBarButton =new Button();
-        postStatusFromStatusBarButton.setLayoutX(800);
-        postStatusFromStatusBarButton.setLayoutY(75);
-        postStatusFromStatusBarButton.setText("Post");
-        postStatusFromStatusBarButton.setPrefSize(60,40);
-        centerPane.getChildren().add(postStatusFromStatusBarButton);
-        postStatusFromStatusBarButton.setOnAction(e->{
-        //implement Post a post here 
-        });
-         
-         
-        CenterPostsPane = new ScrollPane();
-        CenterPostsPane.setLayoutX(10);
-        CenterPostsPane.setLayoutY(120);
-        CenterPostsPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
-        + "-fx-border-width:0;" + "-fx-border-insets: 0;"
-       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #ECF0F1;"+"flex-direction: column;");
-        CenterPostsPane.setPrefSize(Post.getPostWidth()+100,500);
-        CenterPostsPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        //CenterPostsPane.setFitToHeight(true);
-        //CenterPostsPane.setFitToWidth(true);
-        CenterPostsPane.setVmax(1);
-        
-        centerPane.getChildren().add(CenterPostsPane);
-    }
-    void setUpperHMnue()
-    {
-        upperHMenue = new Pane();
-        upperHMenue.setPadding(new Insets(15, 12, 15, 12));
-        
-        
-        userNameLabel = new Label("User_name_Label");
-        userNameLabel.setLayoutX(850);
-        userNameLabel.setLayoutY(10);
-        homeButton = new Button();
-        homeButton.setText("Home");
-        homeButton.setLayoutX(1000);
-        homeButton.setLayoutY(10);
-        homeButton.setPrefSize(100,10);
-        homeButton.setOnAction(e->{
-            //implement "go to home " here
-        });
-        
-        profileButton = new Button();
-        profileButton.setText("Profile");
-        profileButton.setPrefSize(100,10);
-        profileButton.getStyleClass().add("border-blue");
-        profileButton.setLayoutX(1110);
-        profileButton.setLayoutY(10);
-        profileButton.setOnAction(e->{
-            //implement "go to profile " here
-        });
-        
-        searchTxtBox=new TextField();
-        searchTxtBox.setPromptText("search here...");
-        searchTxtBox.setAlignment(Pos.CENTER);
-       searchTxtBox.setLayoutX(500);
-       searchTxtBox.setLayoutY(10);
-       
-        searchButton = new Button();
-        searchButton.setText("Search");
-        searchButton.setLayoutX(500 +5 +150);
-        searchButton.setLayoutY(10);
-        searchButton.setOnAction(e->{
-            //implement search here
-        });
-        
-        upperHMenue.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
-        + "-fx-border-width: 5;" + "-fx-border-insets: 0;"
-        + "-fx-border-radius: 1;" + "-fx-border-color: #AB4642;"+"-fx-background-color: #AB4642;");
-        
-        upperHMenue.getChildren().addAll(userNameLabel,homeButton,profileButton,searchTxtBox,searchButton);
-    }
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Social Network");
-        
-        //set primaryStage boundaries to visible bounds of the main screen
-        primaryStage_boundries_set_to_bounds_of_mainScreen(primaryStage);
-        
-        
         borderPane = new BorderPane ();
         setUpperHMnue();
         borderPane.setTop(upperHMenue);
@@ -189,9 +98,128 @@ public class SocialNetwork extends Application {
         
         CenterPostsPane.setContent(postsGroup);
 //**********************************************************************************************************************************************
-        Scene homePageScene  = new Scene(borderPane);
-        primaryStage.setScene(homePageScene);
-        primaryStage.show();
+        homePageScene  = new Scene(borderPane);
+        return homePageScene;
+    }
+      void setCenterPane()
+    {
+        centerPane = new Pane();
+        centerPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
+        + "-fx-border-width:0;" + "-fx-border-insets: 0;"
+       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #ECF0F1;"+"flex-direction: column;");
+        borderPane.setCenter(centerPane);
+        
+        statusBar = new TextField();
+        statusBar.setPrefSize(1000, 60); 
+        statusBar.setLayoutX(10);
+        statusBar.setLayoutY(10); 
+        statusBar.setPromptText("What's in your mind?...");
+        centerPane.getChildren().add(statusBar);
+        
+        postStatusFromStatusBarButton =new Button();
+        postStatusFromStatusBarButton.setLayoutX(800);
+        postStatusFromStatusBarButton.setLayoutY(75);
+        postStatusFromStatusBarButton.setText("Post");
+        postStatusFromStatusBarButton.setPrefSize(60,40);
+        centerPane.getChildren().add(postStatusFromStatusBarButton);
+        postStatusFromStatusBarButton.setOnAction(e->{
+        //implement Post a post here
+        });
+         
+         
+        CenterPostsPane = new ScrollPane();
+        CenterPostsPane.setLayoutX(10);
+        CenterPostsPane.setLayoutY(120);
+        CenterPostsPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
+        + "-fx-border-width:0;" + "-fx-border-insets: 0;"
+       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #ECF0F1;"+"flex-direction: column;");
+        CenterPostsPane.setPrefSize(Post.getPostWidth()+100,500);
+        CenterPostsPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        //CenterPostsPane.setFitToHeight(true);
+        //CenterPostsPane.setFitToWidth(true);
+        CenterPostsPane.setVmax(1);
+        
+        centerPane.getChildren().add(CenterPostsPane);
+    }
+      void setUpperHMnue()
+    {
+        upperHMenue = new Pane();
+        upperHMenue.setPadding(new Insets(15, 12, 15, 12));
+        
+        
+        userNameLabel = new Label("User_name_Label");
+        userNameLabel.setLayoutX(850);
+        userNameLabel.setLayoutY(10);
+        homeButton = new Button();
+        homeButton.setText("Home");
+        homeButton.setLayoutX(1000);
+        homeButton.setLayoutY(10);
+        homeButton.setPrefSize(100,10);
+        homeButton.setOnAction(e->{
+            //implement "go to home " here
+        });
+        
+        profileButton = new Button();
+        profileButton.setText("Profile");
+        profileButton.setPrefSize(100,10);
+        profileButton.getStyleClass().add("border-blue");
+        profileButton.setLayoutX(1110);
+        profileButton.setLayoutY(10);
+        profileButton.setOnAction(e->{
+            //implement "go to profile " here
+            window.setScene(profile(getCurrentUser()));
+            window.show();
+            
+        });
+        
+        searchTxtBox=new TextField();
+        searchTxtBox.setPromptText("search here...");
+        searchTxtBox.setAlignment(Pos.CENTER);
+       searchTxtBox.setLayoutX(500);
+       searchTxtBox.setLayoutY(10);
+       
+        searchButton = new Button();
+        searchButton.setText("Search");
+        searchButton.setLayoutX(500 +5 +150);
+        searchButton.setLayoutY(10);
+        searchButton.setOnAction(e->{
+            //implement search here
+        });
+        
+        upperHMenue.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
+        + "-fx-border-width: 5;" + "-fx-border-insets: 0;"
+        + "-fx-border-radius: 1;" + "-fx-border-color: #AB4642;"+"-fx-background-color: #AB4642;");
+        
+        upperHMenue.getChildren().addAll(userNameLabel,homeButton,profileButton,searchTxtBox,searchButton);
+    }
+   //************************************************************************************   
+      
+ //***********Profile Page functions******************************************************
+    public static Scene profile (user x)
+    {
+        BorderPane profileBorderPane = new BorderPane();
+        Scene Profile=new Scene(profileBorderPane,1000, 1000);
+       // Profile.getStylesheets().add("Styles.css");
+        return Profile;
+    }
+    //**********************************************************************************
+   
+    user getCurrentUser()
+    {
+        user loggedInUser = new user();
+        //implement  get Current User Here;
+        return loggedInUser;
+    }
+    
+    
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        primaryStage.setTitle("Social Network");
+        primaryStage_boundries_set_to_bounds_of_mainScreen(primaryStage);//set primaryStage boundaries to visible bounds of the main screen
+        window.setScene(homePage(getCurrentUser()));
+        window.show();
       
     }
     private static void primaryStage_boundries_set_to_bounds_of_mainScreen(Stage primaryStage)
@@ -203,5 +231,7 @@ public class SocialNetwork extends Application {
         primaryStage.setHeight(primaryScreenBounds.getHeight());
         setUserAgentStylesheet(STYLESHEET_CASPIAN);
     }
-    
+  
 }
+
+
