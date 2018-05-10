91314ba74742b98cb08c5e9305f19d6125191401
Adel Mahmoud <33881557+adelmaek@users.noreply.github.com>
2018-05-10 20:14:05 +0200

    Update HomePage.java

--- Final Codes/HomePage.java
+++ Final Codes/HomePage.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package socialnetwork;
 
+import java.util.Collections;
 import java.util.Vector;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -18,6 +14,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import  socialnetwork.SocialNetwork;
 
 
@@ -39,7 +36,7 @@ public class HomePage {
      BorderPane  borderPane;
      Pane centerPane;
      ScrollPane CenterPostsPane;
-     VBox centerVBoxPane;
+     VBox centerVBoxPane = new VBox();
      Group postsGroup;
      Button searchButton;
      Scene homePageScene;
@@ -50,6 +47,8 @@ public class HomePage {
      ScrollPane showScrollPane;
      static boolean  friendsListButtonPressed ;
      static boolean groupsListButtonPressed;
+     
+     Label sna;
      
      public HomePage()
      {}
@@ -71,9 +70,9 @@ public class HomePage {
         centerPane = new Pane();
         centerPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
         + "-fx-border-width:0;" + "-fx-border-insets: 0;"
-       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #ECF0F1;"+"flex-direction: column;");
+       + "-fx-border-radius: 0;" + "-fx-border-color: #ECF0F1;"+"-fx-background-color: #B3B6B7;"+"flex-direction: column;");
        
-        
+        //fillScrollPaneWithPosts();
         
         statusBar = new TextField();
         statusBar.setPrefSize(1000, 60); 
@@ -81,6 +80,7 @@ public class HomePage {
         statusBar.setLayoutY(10); 
         statusBar.setPromptText("What's in your mind?...");
         
+        fillScrollPaneWithPosts();
         
         postStatusFromStatusBarButton =new Button();
         postStatusFromStatusBarButton.setLayoutX(800);
@@ -89,14 +89,16 @@ public class HomePage {
         postStatusFromStatusBarButton.setPrefSize(60,40);
         postStatusFromStatusBarButton.setOnAction(e->{
         //implement Post a post here
-        Post p = new Post(statusBar.getText(),SocialNetwork.currentUser.getUsername());
-        SocialNetwork.currentUser.add_post(p);
+        
+        Post x =new Post(statusBar.getText(),SocialNetwork.currentUser.getUsername());
+        SocialNetwork.currentUser.add_post(x);
         fillScrollPaneWithPosts();
+        statusBar.setText("");
         });
          
          
         CenterPostsPane = new ScrollPane();
-        CenterPostsPane.setLayoutX(10);
+        CenterPostsPane.setLayoutX(150);
         CenterPostsPane.setLayoutY(120);
         CenterPostsPane.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
         + "-fx-border-width:0;" + "-fx-border-insets: 0;"
@@ -115,21 +117,28 @@ public class HomePage {
     {
         upperHMenue = new Pane();
         upperHMenue.setPadding(new Insets(15, 12, 15, 12));
-        
+       
+        sna = new Label();
+        sna.setText("Social Network");
+        sna.setStyle("-fx-font:40px \"Serif\";\n" + "-fx-text-fill: white;");
+        sna.setLayoutX(10);
+        sna.setLayoutY(10);
         
         userNameLabel = new Label();
         userNameLabel.setText(SocialNetwork.currentUser.getUsername());
         userNameLabel.setLayoutX(850);
         userNameLabel.setLayoutY(10);
-        userNameLabel.setStyle("-fx-background-color: #AB4642;"+"-fx-padding:2;");
+        //userNameLabel.setPrefWidth(50);
+        userNameLabel.setStyle("-fx-font:40px \"Serif\";\n"+"-fx-text-fill: white;"+"-fx-padding:2;"+"font-weight: bold;");
         homeButton = new Button();
         homeButton.setText("Home");
         homeButton.setLayoutX(1000);
-        homeButton.setLayoutY(10);
+        homeButton.setLayoutY(20);
         homeButton.setPrefSize(100,10);
         homeButton.setOnAction(e->{
             //implement "go to home " here user tempUser = SocialNetwork.currentUser;
             fillScrollPaneWithPosts();
+            
         });
         
         profileButton = new Button();
@@ -137,7 +146,7 @@ public class HomePage {
         profileButton.setPrefSize(100,10);
         profileButton.getStyleClass().add("border-blue");
         profileButton.setLayoutX(1110);
-        profileButton.setLayoutY(10);
+        profileButton.setLayoutY(20);
         profileButton.setOnAction(e->{
             //implement "go to profile " here
             SocialNetwork.window.setScene(Profile.Profile(SocialNetwork.currentUser));             
@@ -148,14 +157,14 @@ public class HomePage {
         searchTxtBox.setPromptText("search here...");
         searchTxtBox.setAlignment(Pos.CENTER);
        searchTxtBox.setLayoutX(500);
-       searchTxtBox.setLayoutY(10);
+       searchTxtBox.setLayoutY(20);
        String[] autocomp = {"Adel","Sama","Salma"};
        
        
         searchButton = new Button();
         searchButton.setText("Search");
         searchButton.setLayoutX(500 +5 +150);
-        searchButton.setLayoutY(10);
+        searchButton.setLayoutY(20);
         searchButton.setOnAction(e->{
             //implement search here
             if (SocialNetwork.searchUsersHashTable(searchTxtBox.getText())!= null)
@@ -175,16 +184,18 @@ public class HomePage {
             
         });
         
+        upperHMenue.setPrefHeight(80);
         upperHMenue.setStyle("-fx-padding:5;" + "-fx-border-style:solid;"
         + "-fx-border-width: 5;" + "-fx-border-insets: 0;"
-        + "-fx-border-radius: 1;" + "-fx-border-color: #AB4642;"+"-fx-background-color: #AB4642;");
+        + "-fx-border-radius: 1;" + "-fx-border-color: #979A9A;"+"-fx-background-color: #979A9A;");
         
-        upperHMenue.getChildren().addAll(userNameLabel,homeButton,profileButton,searchTxtBox,searchButton);
+        upperHMenue.getChildren().addAll(userNameLabel,homeButton,profileButton,searchTxtBox,searchButton,sna);
     }
    //************************************************************************************ 
       VBox peopleYouMayKnow(user currentUser)
       {
           // implement people you may know
+          showVBox.getChildren().clear();
          VBox vB = new VBox();
          return vB;
       }
@@ -193,6 +204,7 @@ public class HomePage {
          showVBox = new VBox();
         showVBox.setPadding(new Insets(15, 12, 15, 12));
         showVBox.setSpacing(10);
+        
        showVBox = peopleYouMayKnow(SocialNetwork.currentUser);
         
         imageView = new ImageView(SocialNetwork.currentUser.getProfilePicture());
@@ -206,27 +218,20 @@ public class HomePage {
         friendsListButton.setText("My Friends");
         friendsListButton.setPrefSize(250,10);
         friendsListButton.setOnAction(e->{
-            if(friendsListButtonPressed)
-            {
-               showVBox = peopleYouMayKnow(SocialNetwork.currentUser);
-                friendsListButtonPressed = false;
-            }
-            else
-            {
-                 Vector<String> myFriendsList = SocialNetwork.currentUser.getFriends();
-                Button [] myFriendButton = new Button[SocialNetwork.currentUser.getNumberOfFriends()];
-                ImageView [] myFriendImageView = new ImageView[SocialNetwork.currentUser.getNumberOfFriends()];
-                VBox [] myFriendVBox = new VBox[SocialNetwork.currentUser.getNumberOfFriends()];
+            
+               showVBox.getChildren().clear();
+                Button [] myFriendButton = new Button[SocialNetwork.currentUser.getFriends().size()];
+                ImageView [] myFriendImageView = new ImageView[SocialNetwork.currentUser.getFriends().size()];
+                VBox [] myFriendVBox = new VBox[SocialNetwork.currentUser.getFriends().size()];
                 int i= 0;
-                for(String aFriendString:myFriendsList )
+                for(String aFriendString:SocialNetwork.currentUser.getFriends() )
                     {
-                        user aFriend = SocialNetwork.searchUsersHashTable(aFriendString);
-                         myFriendImageView[i] = new ImageView(aFriend.getProfilePicture());
+                         myFriendImageView[i] = new ImageView(SocialNetwork.searchUsersHashTable(aFriendString).getProfilePicture());
                          myFriendButton[i] = new Button();
-                         myFriendButton[i].setText(aFriend.getUsername());
+                         myFriendButton[i].setText(SocialNetwork.searchUsersHashTable(aFriendString).getUsername());
                
                         myFriendButton[i].setOnAction(ee->{
-                                 SocialNetwork.window.setScene(Profile.Profile(aFriend)); // Replace with marks function
+                                 SocialNetwork.window.setScene(Profile.Profile(SocialNetwork.searchUsersHashTable(aFriendString))); // Replace with marks function
                             });
                         myFriendVBox[i] = new VBox();
                         myFriendVBox[i].setSpacing(5);
@@ -234,47 +239,32 @@ public class HomePage {
                          showVBox.getChildren().add(myFriendVBox[i]);
                          i++;
                      }
-                friendsListButtonPressed = true;
-             }
         });
         
         groupsListButton = new Button();
         groupsListButton.setText("My Groups");
         groupsListButton.setPrefSize(250,10);
         groupsListButton.setOnAction(e->{
-            if(groupsListButtonPressed)
-            {
-               showVBox = peopleYouMayKnow(SocialNetwork.currentUser);
-                groupsListButtonPressed = false;
-            }
-            else
-            {
-                // implement myGroups
-               // Vector<Group> myGroupList = 
-                Vector<String> myGroupList = SocialNetwork.currentUser.getGroups_names();
+                showVBox.getChildren().clear();
                 Button [] myGroupsButton = new Button[SocialNetwork.currentUser.getGroups_names().size()];
                 ImageView [] myGroupsImageView = new ImageView[SocialNetwork.currentUser.getGroups_names().size()];
                 VBox [] myGroupsVBox = new VBox[SocialNetwork.currentUser.getGroups_names().size()];
                 int i= 0;
-                for(String aGroupString :myGroupList )
+                for(String aGroupString :SocialNetwork.currentUser.getGroups_names() )
                     {
-                        socialnetwork.Group aGroup = SocialNetwork.searchGroupsHashTable(aGroupString);
-                         myGroupsImageView[i] = new ImageView(aGroup.getGroup_pic());
+                         myGroupsImageView[i] = new ImageView(SocialNetwork.searchGroupsHashTable(aGroupString).getGroup_pic());
                          myGroupsButton[i] = new Button();
-                         myGroupsButton[i].setText(aGroup.getName());
+                         myGroupsButton[i].setText(SocialNetwork.searchGroupsHashTable(aGroupString).getName());
                
                         myGroupsButton[i].setOnAction(ee->{
-                                 SocialNetwork.window.setScene(socialnetwork.Group.Groups(aGroup)); // Replace with marks function 
+                                 SocialNetwork.window.setScene(socialnetwork.Group.Groups(SocialNetwork.searchGroupsHashTable(aGroupString))); // Replace with marks function 
                             });
                         myGroupsVBox[i] = new VBox();
                         myGroupsVBox[i].setSpacing(5);
                          myGroupsVBox[i].getChildren().addAll(myGroupsImageView[i],myGroupsButton[i]);
                          showVBox.getChildren().add(myGroupsVBox[i]);
                          i++;
-                     }
-                groupsListButtonPressed = true;
-             }
-            
+                     }            
         });
         
         
@@ -283,7 +273,7 @@ public class HomePage {
         showScrollPane.setContent(showVBox);
         
         leftVMenu = new VBox();
-        leftVMenu.setStyle("-fx-background-color: #AB4642");
+        leftVMenu.setStyle("-fx-background-color: #B3B6B7");
         leftVMenu.setPadding(new Insets(15, 12, 15, 12));
         leftVMenu.setSpacing(10);
         leftVMenu.getChildren().addAll(imageView,groupsListButton,friendsListButton,showScrollPane);
@@ -292,19 +282,42 @@ public class HomePage {
      private void fillScrollPaneWithPosts()
     {
         Post tempPost;
-            tempPost = new Post(SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-1).getPostContent(),SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-1).getPostOwner());
+        int index= 0;
+        boolean filledFlag =false;
+        
+        centerVBoxPane.getChildren().clear();
+       
+        while(Integer.compare(index, SocialNetwork.currentUser.getPosts().size())==-1 )
+        {
+            tempPost = new Post(SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-1-index).getPostContent(),SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-1).getPostOwner());
             centerVBoxPane.getChildren().add(tempPost.getPostArea());
-            tempPost = new Post(SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-2).getPostContent(),SocialNetwork.currentUser.getPosts().get(SocialNetwork.currentUser.getPosts().size()-2).getPostOwner());
-            centerVBoxPane.getChildren().add(tempPost.getPostArea());
-            Vector <String> tempFriendList = SocialNetwork.currentUser.getFriends_names();
-            for(String x : tempFriendList)
+            index++;
+            filledFlag =true;
+            if(index==2)break;
+        }
+       
+       if(Integer.compare(SocialNetwork.currentUser.getFriends().size(), 0)==1)
+       {
+            for(String x : SocialNetwork.currentUser.getFriends_names())
             {
                user tempUser = SocialNetwork.searchUsersHashTable(x);
-               Vector<Post> tempPosts = tempUser.getPosts();
-               tempPost = new Post(tempPosts.get(tempPosts.size()-1).getPostContent(),tempPosts.get(tempPosts.size()-1).getPostOwner());
+               index=0;
+              
+               while(Integer.compare(index, SocialNetwork.searchUsersHashTable(x).posts.size())==-1)
+               {
+               tempPost = new Post(SocialNetwork.searchUsersHashTable(x).posts.get(SocialNetwork.searchUsersHashTable(x).posts.size()-1-index).getPostContent(),SocialNetwork.searchUsersHashTable(x).posts.get(SocialNetwork.searchUsersHashTable(x).posts.size()-1-index).getPostOwner());
                centerVBoxPane.getChildren().add(tempPost.getPostArea());
-               tempPost = new Post(tempPosts.get(tempPosts.size()-2).getPostContent(),tempPosts.get(tempPosts.size()-2).getPostOwner());
-               centerVBoxPane.getChildren().add(tempPost.getPostArea());
+               filledFlag = true;
+               index ++;
+               
+               }
             } 
+        }
+        if(!filledFlag)
+        {
+            Label unfilled = new Label();
+            unfilled.setText("No posts to show");
+            centerVBoxPane .getChildren().add(unfilled);
+        }
     }
 }
