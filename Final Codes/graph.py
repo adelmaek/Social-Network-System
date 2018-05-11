3f7d4a077aab60fe9636df697cc0f1c32f39e62e
SamaElBaroudy <33029828+SamaElBaroudy@users.noreply.github.com>
2018-05-11 22:36:18 +0200

    Add files via upload

--- Final Codes/graph.py
+++ Final Codes/graph.py
@@ -1,5 +1,5 @@
 import json
-import random 
+import randomcolor
 from pprint import pprint
 import networkx as nx
 try:
@@ -25,7 +25,10 @@ for i in range (len(data)):
     name = data[i]['Users']['Username']
     for j in range (len(data[i]['Users']['Friends'])):
         graph.add_edge(data[i]['Users']['Username'],data[i]['Users']['Friends'][j])
-        
-nx.draw(graph,node_color='#A0CBE2',edge_color='#A0CB77',width=2,with_labels = True)
+
+for i in range (len(nx.nodes(graph))):
+    rand_color = randomcolor.RandomColor()
+    
+nx.draw(graph,node_color='#cc6699',font_size=10,node_size=1500,edge_color='#000066',width=2,with_labels = True)
 plt.savefig("graph.png") # save as png
 plt.show() # display
